71df592f83b212b17f7019f9a8000785ea68b309
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-14 21:41:30 +0100

    :hammer: Use a `denoify` script to fix errors

---
+++ scripts/denoify.ts
@@ -0,0 +1,29 @@
+import { fs, path } from '../src';
+
+const directoryPath = path.resolve(import.meta.dir, '..', 'jsr');
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    throw err;
+  }
+
+  for (const file of files) {
+    if (file.endsWith('.ts')) {
+      const filePath = path.join(directoryPath, file);
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        const result = data.replace(/node:node:/g, 'node:');
+
+        fs.writeFile(filePath, result, 'utf8', (err) => {
+          if (err) {
+            throw err;
+          }
+          console.log('[\x1b[32m+\x1b[0m]', `${file} denoified`);
+        });
+      });
+    }
+  }
+});
