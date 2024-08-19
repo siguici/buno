0fe2801c41b2506e17a6015c683e0bc950884472
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-19 04:22:19 +0100

    :pencil2: Fix: export Response, Headers from fetch API

--- src/fetch.ts
+++ src/fetch.ts
@@ -3,4 +3,4 @@ const _Request = Request;
 const _Response = Response;
 const _Headers = Headers;
 
-export { _fetch as fetch, _Request as Request, _Response, _Headers };
+export { _fetch as fetch, _Request as Request, _Response as Response, _Headers as Headers };
