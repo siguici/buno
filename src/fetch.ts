c7a0a734faf75e72800b1446f73302a4f95670f8
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-19 09:28:13 +0100

    :rotating_light: Fix code format

--- src/fetch.ts
+++ src/fetch.ts
@@ -3,4 +3,9 @@ const _Request = Request;
 const _Response = Response;
 const _Headers = Headers;
 
-export { _fetch as fetch, _Request as Request, _Response as Response, _Headers as Headers };
+export {
+  _fetch as fetch,
+  _Request as Request,
+  _Response as Response,
+  _Headers as Headers,
+};
