b9935587f1a0eb5ecb198d137df03decd9b556ce
⚡ Sigui <siguici@proton.me>
2025-01-24 19:09:32 +0100

    🧑‍💻 Make the `which` API compatible with Bun's

--- src/which.ts
+++ src/which.ts
@@ -1,5 +1,43 @@
-import which from 'which';
+import _which from 'which';
 
-const whichSync = which.sync;
+const _whichSync = _which.sync;
+
+const which = async (
+  command: string,
+  options: { PATH?: string; cwd?: string },
+): Promise<string | null> => {
+  const opts = { nothrow: true, path: options.PATH };
+  let result: string | null;
+
+  if (options.cwd) {
+    const owd = process.cwd();
+    process.chdir(options.cwd);
+    result = await _which(command, opts);
+    process.chdir(owd);
+  } else {
+    result = _whichSync(command, opts);
+  }
+
+  return result;
+};
+
+const whichSync = (
+  command: string,
+  options: { PATH?: string; cwd?: string },
+): string | null => {
+  const opts = { nothrow: true, path: options.PATH };
+  let result: string | null;
+
+  if (options.cwd) {
+    const owd = process.cwd();
+    process.chdir(options.cwd);
+    result = _whichSync(command, opts);
+    process.chdir(owd);
+  } else {
+    result = _whichSync(command, opts);
+  }
+
+  return result;
+};
 
 export { which, whichSync };
