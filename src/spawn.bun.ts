cfa3dab7a76320f4feefd47d14cad3a610ef70d5
⚡ Sigui <siguici@proton.me>
2025-02-01 19:45:38 +0100

    ✨ Add Bun exec()

---
+++ src/spawn.bun.ts
@@ -0,0 +1,81 @@
+import type { SpawnOptions, Subprocess } from 'bun';
+import type {
+  ExecOptions as BenoExecOptions,
+  ExecCallback,
+  ExecResult,
+} from './types';
+
+export type ExecOptions = BenoExecOptions & SpawnOptions.OptionsObject;
+
+export function exec(command: string | string[]): ExecResult;
+export function exec(
+  command: string | string[],
+  options: ExecOptions,
+): ExecResult;
+export function exec<T = void>(
+  command: string | string[],
+  options: ExecOptions,
+  callback: ExecCallback<T>,
+): ExecResult;
+export function exec<T = void>(
+  command: string | string[],
+  options?: ExecOptions,
+  callback?: ExecCallback<T>,
+): ExecResult {
+  if (options !== undefined) {
+    options.stdio = ['ignore', 'pipe', 'pipe'];
+  }
+  if (callback) {
+    if (options === undefined) {
+      options = {};
+    }
+    options.onExit = (
+      proc: Subprocess<'ignore', 'pipe', 'pipe'>,
+      exitCode: number,
+      signalCode: number | null,
+      error: Error | undefined,
+    ) => {
+      const stdio = {
+        stdout: proc.stdout.toString(),
+        stderr: proc.stderr.toString(),
+      };
+      const cbOpts =
+        exitCode === 0
+          ? {
+              code: 0,
+              signal: signalCode,
+              success: true,
+              failed: false,
+              error: null,
+              ...stdio,
+            }
+          : {
+              code: exitCode,
+              signal: signalCode,
+              success: false,
+              failed: true,
+              error:
+                error ||
+                new Error(
+                  `Process exited with code ${exitCode} signal ${signalCode}`,
+                ),
+              ...stdio,
+            };
+      callback(cbOpts);
+    };
+  }
+  return new Promise((resolve) => {
+    const child = Bun.spawn(
+      Array.isArray(command) ? command : command.split(/\s+/),
+      options,
+    );
+    const code = (child.exitCode ?? child.stderr) ? 1 : 0;
+    resolve({
+      code,
+      stdout: child.stdout.toString(),
+      stderr: child.stderr,
+      success: code === 0,
+      failed: code !== 0,
+    });
+  });
+}
