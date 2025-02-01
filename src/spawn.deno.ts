1224c9ff642a3ab7c34e4b841c553a9bc179ba1f
⚡ Sigui <siguici@proton.me>
2025-02-01 19:46:16 +0100

    ✨ Add Deno exec()

---
+++ src/spawn.deno.ts
@@ -0,0 +1,54 @@
+import { stderr } from 'node:process';
+import type {
+  ExecOptions as BenoExecOptions,
+  ExecCallback,
+  ExecResult,
+} from './types';
+
+export type ExecOptions = BenoExecOptions & Deno.CommandOptions;
+
+export async function exec(command: string | string[]): ExecResult;
+export async function exec(
+  command: string | string[],
+  options: ExecOptions,
+): ExecResult;
+export async function exec<T = void>(
+  command: string | string[],
+  options: ExecOptions,
+  callback: ExecCallback<T>,
+): ExecResult;
+export async function exec<T = void>(
+  command: string | string[],
+  options?: ExecOptions,
+  callback?: ExecCallback<T>,
+): ExecResult {
+  let args = Array.isArray(command) ? command : command.split(/\s+/);
+  command = args[0];
+  args = args.slice(1);
+
+  const proc = new Deno.Command(command, {
+    args,
+    ...options,
+  });
+
+  if (callback) {
+    const output = await proc.output();
+    const result = {
+      code: output.code,
+      success: output.success,
+      failed: !output.success,
+      stderr: output.stderr.toString(),
+      stdout: output.stdout.toString(),
+    };
+    callback(result);
+
+    return result;
+  }
+  return proc.output().then((output) => ({
+    code: output.code,
+    stdout: output.stdout.toString(),
+    stderr: output.stderr.toString(),
+    success: output.success,
+    failed: !output.success,
+  }));
+}
